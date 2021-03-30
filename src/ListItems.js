590a99172c61447781d794dd711f7c2cf30aad8d
Farish.py.js <67537384+Farish3444@users.noreply.github.com>
2021-03-31 01:43:11 +0530

    Add files via upload

---
+++ src/ListItems.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ListItems.css';
+
+function ListItems(props){
+    const items=props.items;
+    const ListItems= items.map(items=>{
+        return <div className="List" key="item.key">
+            <p>{items.text}</p>
+        </div>
+    })
+
+    return(
+        <div>{ListItems}</div>
+      
+    )
+
+}
+
+export default ListItems;
